a2613910cb172ccfa152305396aa64d0faa4473b
Evan Hernandez <evanfredhernandez@gmail.com>
2022-12-02 19:13:34 -0500

    Fix a few new typing errors.

--- src/utils/typing.py
+++ src/utils/typing.py
@@ -6,6 +6,7 @@ import datasets
 import numpy
 import torch
 import transformers
+import transformers.modeling_outputs
 
 ArrayLike = list | tuple | numpy.ndarray | torch.Tensor
 PathLike = Union[str, pathlib.Path]
@@ -18,8 +19,8 @@ Tokenizer = transformers.PreTrainedTokenizerFast
 TokenizerOffsetMapping = Sequence[tuple[int, int]]
 Dataset = datasets.arrow_dataset.Dataset | datasets.dataset_dict.DatasetDict
 ModelInput = transformers.BatchEncoding
-ModelOutput = transformers.modeling_outputs.BaseModelOutputWithPastAndCrossAttentions
-ModelGenerateOutput = transformers.generation_utils.GenerateOutput | torch.LongTensor
+ModelOutput = transformers.modeling_outputs.CausalLMOutput
+ModelGenerateOutput = transformers.generation.utils.GenerateOutput | torch.LongTensor
 
 # All strings are also Sequence[str], so we have to distinguish that we
 # mean lists or tuples of strings, or sets of strings, not other strings.
