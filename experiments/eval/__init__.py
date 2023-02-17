95130f9834bc154a00b3d82d67548cd8be61fb6f
Evan Hernandez <evanfredhernandez@gmail.com>
2023-02-16 23:01:49 -0600

    Add cls experiments to collection.

--- experiments/eval/__init__.py
+++ experiments/eval/__init__.py
@@ -1,7 +1,8 @@
 """Evaluation experiments."""
-from experiments.eval import gen
+from experiments.eval import cls, gen
 
 from invoke import Collection
 
 ns = Collection()
 ns.add_collection(Collection.from_module(gen))
+ns.add_collection(Collection.from_module(cls))
