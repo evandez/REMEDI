08f9b0af5b5cc5e30d7728ff776fad9feb3b89a2
Evan Hernandez <evanfredhernandez@gmail.com>
2023-03-01 19:52:54 -0500

    Add scripts for dumping directions.

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,8 +1,8 @@
 """Invoke tasks for running experiments from the paper."""
-from experiments import eval, sweep, train
+from experiments import dump, eval, sweep, train
 
 from invoke import Collection
 
 ns = Collection("x")
-for module in (eval, train, sweep):
+for module in (dump, eval, train, sweep):
     ns.add_collection(Collection.from_module(module))
---
+++ experiments/dump.py
@@ -0,0 +1,40 @@
+"""Dump directions."""
+from experiments import utils
+from experiments.aliases import *
+
+from invoke import Collection, task
+
+
+def _dump(c, model, dataset, device=None):
+    editors_dir = utils.require_editors_dir(model=model, dataset=dataset)
+    layer = REMEDI_EDITOR_LAYER[model]
+    cmd = (
+        "python -m scripts.generate_directions "
+        f"-e {editors_dir} -l {layer} -d {dataset} -m {model}"
+    )
+    cmd = utils.maybe_set_device(cmd, device=device)
+    c.run(cmd)
+
+
+@task
+def dump_cf(c, model=DEFAULT_MODEL, device=None):
+    """Generate dump of REMEDI directions for CounterFact."""
+    _dump(c, model, CF, device=device)
+
+
+@task
+def dump_bb(c, model=DEFAULT_MODEL, device=None):
+    """Generate dump of REMEDI directions for Bias in Bios."""
+    _dump(c, model, BB, device=device)
+
+
+@task
+def dump_mc(c, model=DEFAULT_MODEL, device=None):
+    """Generate dump of REMEDI directions for McRae."""
+    _dump(c, model, MC, device=device)
+
+
+ns = Collection()
+ns.add_task(dump_cf, CF)
+ns.add_task(dump_bb, BB)
+ns.add_task(dump_mc, MC)
