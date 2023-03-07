9b4b71be79e27f499a04fbfeec78edc6c04a1db2
Evan Hernandez <evanfredhernandez@gmail.com>
2023-03-07 13:38:14 -0500

    Pretty nasty bug.

--- experiments/eval/ent.py
+++ experiments/eval/ent.py
@@ -8,7 +8,7 @@ from invoke import task
 @task(name=MC)
 def eval_ent_mc(c, model=DEFAULT_MODEL, device=None):
     """Evaluate REMEDI on entailment."""
-    editors_dir = utils.require_editors_dir(model=model, dataset=CF)
+    editors_dir = utils.require_editors_dir(model=model, dataset=MC)
     name = utils.experiment_name("eval_ent_mcrae", model=model)
     layer = REMEDI_EDITOR_LAYER[model][MC]
     cmd = f"python -m scripts.eval_entailment -n {name} -m {model}"
